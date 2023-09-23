a279f0f928c2b77275056a032f53d73e648d1236
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2023-09-23 23:51:26 +0600

    from added

---
+++ reat-router-intro/src/component/Simple-from/Frome.jsx
@@ -0,0 +1,16 @@
+
+import PropTypes from 'prop-types';
+
+const Frome = props => {
+    return (
+        <div>
+            <h1>hello from s</h1>
+        </div>
+    );
+};
+
+Frome.propTypes = {
+
+};
+
+export default Frome;
