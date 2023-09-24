2aed269c368f6b4b63c8508d653e6b8c2bb9de3f
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2023-09-24 22:13:27 +0600

    from added

--- reat-router-intro/src/component/Simple-from/Frome.jsx
+++ reat-router-intro/src/component/Simple-from/Frome.jsx
@@ -4,7 +4,9 @@ import PropTypes from 'prop-types';
 const Frome = props => {
     return (
         <div>
-            <h1>hello from s</h1>
+            <h1>hello from </h1>
+            <input type="text" name="" id="" />
+            <button>Submit</button>
         </div>
     );
 };
